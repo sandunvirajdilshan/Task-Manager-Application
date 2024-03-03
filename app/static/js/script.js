381fb571158b04126bac767508f091da5337a01a
SandunVirajDilshan <sandunvirajdilshan.s@gmail.com>
2024-03-03 14:02:07 +0530

    Updated

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -170,7 +170,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     if (data.success === 'true') {
                         hideLoading()
                         showMessage(data.message, data.success === 'true');
-                        fetchTasks();
                     } else {
                         hideLoading()
                         showMessage(data.message, data.success === 'true');
